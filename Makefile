c07b79642fe889236cbc3819c1693716e86c4819
Alex Monk <am2121@kent.ac.uk>
2019-01-28 12:29:16 +0000

    WIP

--- Makefile
+++ Makefile
@@ -1,19 +1,3 @@
-all: compile
+test: ./run_tests.sh
 
-compile: deps
-	./compile.sh
-
-deps:
-	./install_deps.sh
-	touch deps
-
-test: compile
-	./run_tests.sh
-
-clean:
-	rm -rf classes
-
-reallyclean: clean
-	rm -rf lib deps
-
-.PHONY: all compile test clean reallyclean
+.PHONY: all test
--- src/task.py
+++ src/task.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
 # Author: Alex Monk <am2121@kent.ac.uk>
 # For CO633
-# TODO: make state be a list of strings with package name equals version
-import json
+# Solve seen tests:
+# seen 0: all output states look OK - +1 mark if I can choose one
+# TODO: seen 1: this kills the computer - 0 marks
+# TODO: seen 2: no states output, but all tests should have solutions - 0 marks
+# TODO: seen 3: produces one good result result but not the other, A and D... probably because A has not been installed by the time it considers D? loop? - 0 marks
+# TODO: seen 4: traceback as = state stuff gets into tuple list state format - 0 marks
+# TODO: seen 5: traceback as = state stuff gets into tuple list state format - 0 marks
+# TODO: seen 6: outputs contain a bunch of duplicates packages in each state, last output state does not satisfy all constraints, remaining constraints are duplicates, possibly missing valid states - 0 marks
+# TODO: seen 7: no states output, but all tests should have solutions - 0 marks
+# TODO: seen 8: no states output, but all tests should have solutions - 0 marks
+# TODO: seen 9: traceback as = state stuff gets into tuple list state format - 0 marks
+
+# TODO: Docker
+# TODO: output state in the original format
+# TODO: keep track of installations/removals in each call
+# TODO: assign cost to each potential solution and use it to return best result
 import copy
-with open('repository.json') as f:
+import json
+import sys
+
+with open(sys.argv[1]) as f:
     init_repo_desc = json.load(f)
 
-with open('initial.json') as f:
-    init_state = json.load(f)
+with open(sys.argv[2]) as f:
+    init_state = json.load(f) # TODO: convert into our internal format to handle tests 4, 5 and 9
 
-with open('constraints.json') as f:
+with open(sys.argv[3]) as f:
     init_constraints = json.load(f)
 
-print('Initial repository:', init_repo_desc)
-print('Initial state:', init_state)
-print('Initial constraints:', init_constraints)
+#print('Initial repository:', init_repo_desc)
+#print('Initial state:', init_state)
+#print('Initial constraints:', init_constraints)
 
 def split_namever(namever):
     if '<=' in namever:
@@ -38,7 +55,8 @@ def split_namever(namever):
 
 def is_state_valid(repo_desc, state):
     # not having any conflicts, all dependencies being satisfied
-    print('considering state', state)
+    #print('considering state', state)
+    #print(state)
     for package, version in state:
         for repo_package in repo_desc:
             if package == repo_package['name'] and version == repo_package['version']:
@@ -59,7 +77,7 @@ def is_state_valid(repo_desc, state):
                         if dg_satisfied:
                             break
                     if not dg_satisfied:
-                        print('Dependency missing', repo_package, dependency_group, state)
+                        #print('Dependency missing', repo_package, dependency_group, state)
                         return False
                 break
         else:
@@ -72,7 +90,7 @@ def get_states(repo_desc, state, constraints):
     else:
         constraint = constraints.pop()
         if constraint[0] == '-':
-            print('absent', constraint[1:])
+            #print('absent', constraint[1:])
             to_remove_from_state = []
             namever = constraint[1:]
             name, version_match_f = split_namever(namever)
@@ -84,7 +102,7 @@ def get_states(repo_desc, state, constraints):
                 state.remove(package_to_remove)
             yield from get_states(repo_desc, state, constraints)
         elif constraint[0] == '+':
-            print('present', constraint[1:])
+            #print('present', constraint[1:])
             namever = constraint[1:]
             name, version_match_f = split_namever(namever)
             for package in repo_desc:
@@ -112,4 +130,4 @@ def get_states(repo_desc, state, constraints):
 
 for state in get_states(init_repo_desc, init_state, init_constraints):
     if is_state_valid(init_repo_desc, state):
-        print('end state', state)
+        print(['+{}={}'.format(p, v) for (p, v) in state])
