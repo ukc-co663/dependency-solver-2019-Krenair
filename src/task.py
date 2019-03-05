9b35983e82c9413e683a43a17c1fef8502d66c37
Alex Monk <am2121@kent.ac.uk>
2019-03-05 14:46:01 +0000

    followup previous commit, more tidying and rename new func

--- src/task.py
+++ src/task.py
@@ -50,11 +50,11 @@ def split_namever(namever):
     else:
         return namever, lambda _: True
 
-def find_package_in_repo(repo_desc, namever):
+def find_packages_in_repo(repo_desc, namever):
     name, ver_match_f = split_namever(namever)
     return filter(lambda p: p['name'] == name and ver_match_f(p['version']), repo_desc)
 
-def find_package_in_state(state, namever):
+def find_packages_in_state(state, namever):
     name, ver_match_f = split_namever(namever)
     return filter(lambda p: p[0] == name and ver_match_f(p[1]), state)
 
@@ -66,13 +66,13 @@ def is_state_valid(repo_desc, state):
         for repo_package in repo_desc:
             if package == repo_package['name'] and version == repo_package['version']:
                 for conflict_namever in repo_package.get('conflicts', []):
-                    if len(list(find_package_in_state(state, conflict_namever))):
+                    if len(list(find_packages_in_state(state, conflict_namever))):
                         return False
                 for dependency_group in repo_package.get('depends', []):
                     dg_satisfied = False
                     for dependency_namever in dependency_group:
                         # one of these dependencies in each dependency group must be present
-                        if len(list(find_package_in_state(state, dependency_namever))):
+                        if len(list(find_packages_in_state(state, dependency_namever))):
                             dg_satisfied = True
                             break
                     if not dg_satisfied:
@@ -90,13 +90,7 @@ def get_states(repo_desc, state, constraints):
         constraint = constraints.pop()
         if constraint[0] == '-':
             #print('absent', constraint[1:])
-            to_remove_from_state = []
-            namever = constraint[1:]
-            name, version_match_f = split_namever(namever)
-            for installed_package in state:
-                installed_package_name, installed_package_version = installed_package
-                if installed_package_name == name and version_match_f(installed_package_version):
-                    to_remove_from_state.append(installed_package)
+            to_remove_from_state = list(find_packages_in_state(state, constraint[1:]))
             for package_to_remove in to_remove_from_state:
                 state.remove(package_to_remove)
             for subcommands, substate in get_states(repo_desc, state, constraints):
@@ -104,14 +98,13 @@ def get_states(repo_desc, state, constraints):
         elif constraint[0] == '+':
             #print('present', constraint[1:])
             namever = constraint[1:]
-            for package in find_package_in_repo(repo_desc, namever):
+            for package in find_packages_in_repo(repo_desc, namever):
                 new_package = package['name'], package['version']
                 possible_states = [(['+{}={}'.format(*new_package)], state + [new_package])]
                 for dependency_group in package.get('depends', []):
                     new_possible_states = []
                     for dependency_namever in dependency_group:
-                        dependency_name, dependency_version_match_f = split_namever(dependency_namever)
-                        for potential_dependency_package in find_package_in_repo(repo_desc, dependency_namever):
+                        for potential_dependency_package in find_packages_in_repo(repo_desc, dependency_namever):
                             for possible_commands, possible_state in possible_states:
                                 extra_state = potential_dependency_package['name'], potential_dependency_package['version']
                                 # TODO: add in constraints for dependencies?
