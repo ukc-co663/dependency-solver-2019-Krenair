66f8c84712c519db5cbd178e42b1ab53bba5c54d
Alex Monk <am2121@kent.ac.uk>
2019-03-05 00:26:31 +0000

    update notes

--- src/task.py
+++ src/task.py
@@ -2,17 +2,16 @@
 # Author: Alex Monk <am2121@kent.ac.uk>
 # For CO633
 # Solve seen tests:
-# seen 0: all output states look OK - +1 mark if I can choose one
+# seen 0: all output states look OK - +1 mark
 # TODO: seen 1: this kills the computer - 0 marks
 # TODO: seen 2: no states output, but all tests should have solutions - 0 marks
 # TODO: seen 3: produces one good result result but not the other, A and D... probably because A has not been installed by the time it considers D? loop? - 0 marks
-# TODO: seen 4: traceback as = state stuff gets into tuple list state format - 0 marks
-# TODO: seen 5: traceback as = state stuff gets into tuple list state format - 0 marks
+# TODO: seen 4: no states output, but all tests should have solutions - 0 marks
+# TODO: seen 5: looks good, but could remove initial state B=3 and get lots more pairs such as B=2,A=3 and B=1,A=2 and B=1,A=3 - 0 marks?
 # TODO: seen 6: outputs contain a bunch of duplicates packages in each state, last output state does not satisfy all constraints, remaining constraints are duplicates, possibly missing valid states - 0 marks
 # TODO: seen 7: no states output, but all tests should have solutions - 0 marks
 # TODO: seen 8: no states output, but all tests should have solutions - 0 marks
-# TODO: seen 9: traceback as = state stuff gets into tuple list state format - 0 marks
-
+# seen 9: might work - +1 mark
 # TODO: output each state in the right order?
 # TODO: keep track of installations/removals in each call
 # TODO: assign cost to each potential solution and use it to return best result
